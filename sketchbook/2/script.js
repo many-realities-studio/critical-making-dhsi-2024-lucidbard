9aa3072a66059176e5745fbbff93a3ecba24a344
2390806+lucidbard@users.noreply.github.com <2390806+lucidbard@users.noreply.github.com>
2024-06-06 16:13:36 +0000

    Jun 6, 2024, 9:13 AM

--- sketchbook/2/script.js
+++ sketchbook/2/script.js
@@ -1,5 +1,5 @@
 // @ts-check
-/// <reference path="../node_modules/@types/p5/global.d.ts" />
+/// <reference path="../../node_modules/@types/p5/global.d.ts" />
 
 function setup() {
   createCanvas(400, 400);
